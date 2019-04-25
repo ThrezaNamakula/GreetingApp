c462bbbef9670757b9cfc26cbbc581547944307a
Lubanga Hakim <lubangahakim2@gmail.com>
2019-04-25 13:32:01 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://preply.com/wp-content/uploads/2018/09/greetings_hug.jpg" className="App-logo" alt="hug" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Greeting App</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              </header>
+<div className = "PageHeader"/>
+<div className ="Timeteller"/>
     </div>
   );
 }
