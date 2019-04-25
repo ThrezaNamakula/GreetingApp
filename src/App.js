29d5662dd404f0f6dc171824f2031c6970f340cb
Lubanga Hakim <lubangahakim2@gmail.com>
2019-04-25 13:32:14 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
+import Greetings from "./Greetings";
+import "./App.css" 
+ 
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentPage: "home",
+            name:""
+        }
+        this.goToPage = this.goToPage.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-import './App.css';
+    goToPage(page) {
+        this.setState({currentPage: page});
+    }
+    handleChange = event => {
+        this.setState({ username: event.target.value });
+      };
+     
+  render() {
+    if (this.state.currentPage === "home") 
+       
+    return (
+      <div className="greetingsMain">
+        <div className="header">
+          <form onSubmit = {() => this.goToPage("greetings")}>
+            <input placeholder="Type your name here"name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}>
+            </input>
+            <button type="submit">Open</button>
+          </form>
+        </div>
+      </div>
+    );
+    else if (this.state.currentPage === "greetings") {
+        return <Greetings name={this.state.username} gotoPage={this.goToPage}/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://preply.com/wp-content/uploads/2018/09/greetings_hug.jpg" className="App-logo" alt="hug" />
-        <p>
-          <h1>Greeting App</h1>
-        </p>
-              </header>
-<div className = "PageHeader"/>
-<div className ="Timeteller"/>
-    </div>
-  );
+    }
+  }
 }
-
+ 
 export default App;
---
+++ src/Greetings.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./Greeting.css";
+
+ 
+class Greetings extends Component {
+   
+        constructor(props) {
+          super(props);
+          this.state = {
+                date: '',
+                
+          };
+          
+        }
+        componentDidMount() {
+         
+           
+          var hours = new Date().getHours(); //Current Hours
+          var min = new Date().getMinutes(); //Current Minutes
+         
+          this.setState({
+            //Setting the value of the date time
+            date:
+              'It is ' + hours + ':' + min + 'hrs',
+              hours: hours,
+              min: min
+              
+          });
+        }
+
+    getPartOfDay() {
+        if (this.state.hours < 12) {
+            return "Morning";
+        } else {
+            return "Afternoon"
+        }
+    }
+
+
+  render() {
+   
+       
+        return (
+      <div className="greetingsMain">
+        <div className="header">
+          <h1>Good {this.getPartOfDay()} {this.props.name}!</h1>
+          <h2>{this.state.date}</h2>
+          <p>Enjoy your {this.getPartOfDay()}</p>
+          <form  >
+            <button type="submit" onClick={() => this.props.gotoPage("home")}>Back</button>
+          </form>
+        </div>
+      </div>
+    );
+    
+  }
+}
+ 
+export default Greetings;
